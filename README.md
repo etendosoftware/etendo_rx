c2cc0565c536957000197b26064ea36974d18fad
Sebastian Barrozo <sebastian.barrozo@smfconsulting.es>
2022-07-11 16:54:47 -0300

    Feature EPL-261: Cleanup directories
    Feature EPL-284 convert generate entities to task

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Etendo RX
+### Gradle properties
+    Rename the 'gradle.properties.template' to 'gradle.properties'
+
+### Generation of entities
+
+- To generate the entities run
+
+```./gradlew generate.entities --info```
+
+> The properties will be taken from the 'gradle.properties' file.
+
+
+### Project properties
+
+You can set custom properties when running a project to override the default ones.
+
+Example:
+```
+./gradlew com.etendorx.das:bootRun --info --args='--spring.datasource.url=jdbc:postgresql://localhost:5470/etendo'
+```
--- modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/JwtauthApplication.java
+++ modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/JwtauthApplication.java
@@ -3,9 +3,11 @@ package com.etendorx.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableFeignClients
+@ComponentScan(basePackages = {"com.etendorx.clientrest.*", "com.etendorx.auth.*"})
+@EnableFeignClients(basePackages = {"com.etendorx.clientrest.*", "com.etendorx.auth.clientrest", "com.etendorx.auth.feign"})
 public class JwtauthApplication {
 
 	public static void main(String[] args) {
