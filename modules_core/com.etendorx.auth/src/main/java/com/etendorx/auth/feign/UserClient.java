30fda1582f9e1febf44c4f87947ca78bbaab9ce8
nicolasgabrielr <nicolas.re@smfconsulting.es>
2022-09-06 16:05:23 -0300

    Feature EPL-387: Fix to entity name generation (Auth)

--- modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/feign/UserClient.java
+++ modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/feign/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="users", url="${das.url}/adUsers")
+@FeignClient(name="users", url="${das.url}/ADUser")
 public interface UserClient {
 
     @GetMapping("/search/searchByUsername?username={username}")
