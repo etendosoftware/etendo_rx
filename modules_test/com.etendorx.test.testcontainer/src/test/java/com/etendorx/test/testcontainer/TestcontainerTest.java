7e375fe9e516d0068db6f3afd6a3cf7a9035148d
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-06-14 09:06:08 -0300

    Feature EPL-683: Adding testcontainer test

---
+++ modules_test/com.etendorx.test.testcontainer/src/test/java/com/etendorx/test/testcontainer/TestcontainerTest.java
@@ -0,0 +1,35 @@
+package com.etendorx.test.testcontainer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+@SpringBootTest
+@Testcontainers
+public class TestcontainerTest {
+
+  private static final PostgreSQLContainer<?> postgreSqlContainer = new PostgreSQLContainer<>("postgres:14");
+
+  @BeforeAll
+  static void beforeAll() {
+    postgreSqlContainer.start();
+  }
+
+  @Test
+  void test() {
+    Assertions.assertEquals(1, 1);
+  }
+
+  @DynamicPropertySource
+  static void postgresqlProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgreSqlContainer::getJdbcUrl);
+    registry.add("spring.datasource.password", postgreSqlContainer::getPassword);
+    registry.add("spring.datasource.username", postgreSqlContainer::getUsername);
+  }
+}
