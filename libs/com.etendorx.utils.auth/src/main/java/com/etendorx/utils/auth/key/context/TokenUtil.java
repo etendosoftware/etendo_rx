53e601038d9def26af61180b108b7810a75ed7c5
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-10-18 14:54:08 -0300

    Feature EPL-1044: Fix dependendencies

--- libs/com.etendorx.utils.auth/src/main/java/com/etendorx/utils/auth/key/context/TokenUtil.java
+++ libs/com.etendorx.utils.auth/src/main/java/com/etendorx/utils/auth/key/context/TokenUtil.java
@@ -5,8 +5,8 @@ import com.etendorx.utils.auth.key.JwtKeyUtils;
 import java.util.Map;
 
 public class TokenUtil {
-  public static void convertToken(UserContext userContext, String token) {
-    Map<String, Object> tokenValuesMap = ContextUtils.getTokenValues(token);
+  public static void convertToken(UserContext userContext, String publicKey, String token) {
+    Map<String, Object> tokenValuesMap = ContextUtils.getTokenValues(publicKey, token);
     userContext.setUserId((String) tokenValuesMap.get(JwtKeyUtils.USER_ID_CLAIM));
     userContext.setClientId((String) tokenValuesMap.get(JwtKeyUtils.CLIENT_ID_CLAIM));
     userContext.setOrganizationId((String) tokenValuesMap.get(JwtKeyUtils.ORG_ID));
