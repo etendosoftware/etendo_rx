3bd057ef18ddda02bed3ac2b7f4f08b2e0adbd71
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-10-24 10:45:31 -0300

    Feature EPL-1023: Improve code and add javadocs

--- modules_core/com.etendorx.das/src/main/java/com/etendorx/das/handler/RestCallTransactionHandlerImpl.java
+++ modules_core/com.etendorx.das/src/main/java/com/etendorx/das/handler/RestCallTransactionHandlerImpl.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright 2022-2023  Futit Services SL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.etendorx.das.handler;
 
 import com.etendorx.eventhandler.transaction.RestCallTransactionHandler;
 import com.etendorx.utils.auth.key.context.UserContext;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
@@ -22,15 +34,9 @@ import java.sql.SQLException;
 @Component
 public class RestCallTransactionHandlerImpl implements RestCallTransactionHandler {
   private final boolean isPostgreSQL;
-  @Autowired
-  private TransactionTemplate transactionTemplate;
-
-  @Autowired
-  private DataSource dataSource;
-
+  private final TransactionTemplate transactionTemplate;
+  private final DataSource dataSource;
   private final UserContext userContext;
-  @PersistenceContext
-  EntityManager entityManager;
 
   public RestCallTransactionHandlerImpl(UserContext userContext,
       @Value("${spring.datasource.url}") String datasourceUrl,
@@ -41,39 +47,47 @@ public class RestCallTransactionHandlerImpl implements RestCallTransactionHandle
     this.dataSource = dataSource;
   }
 
-  private void call(String sql) {
+  /**
+   * Must be called on transaction begin.
+   */
+  @Override
+  public void begin() {
+    executeStatement(getDisableStatement());
+  }
+
+  /**
+   * Must be called on transaction commit. This method will enable triggers.
+   */
+  @Override
+  @Transactional(Transactional.TxType.REQUIRES_NEW)
+  public void commit() {
+    executeStatement(getEnableStatement());
+  }
+
+  /**
+   * Executes the given SQL statement in case the user context has triggers disabled.
+   *
+   * @param sql
+   */
+  private void executeStatement(String sql) {
     if (!userContext.isTriggerEnabled()) {
       transactionTemplate.executeWithoutResult(status -> {
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        try {
+        try (Connection connection = DataSourceUtils.getConnection(dataSource)) {
           var stmt = connection.prepareStatement(sql);
           stmt.execute();
         } catch (SQLException e) {
-          throw new RuntimeException(e);
+          throw new RuntimeException("An error occurred while executing the SQL statement", e);
         }
       });
     }
   }
 
-  @Override
-  public void begin() {
-    this.call(getDisableStatement());
-  }
-
-  @Override
-  @Transactional(Transactional.TxType.REQUIRES_NEW)
-  public void commit() {
-    transactionTemplate.executeWithoutResult(status -> {
-      Connection connection = DataSourceUtils.getConnection(dataSource);
-      try {
-        var stmt = connection.prepareStatement(getEnableStatement());
-        stmt.execute();
-      } catch (SQLException e) {
-        throw new RuntimeException("An error has ocurred while enabling triggers", e);
-      }
-    });
-  }
-
+  /**
+   * Returns the SQL statement to disable triggers. This statement is different depending on the
+   * database engine.
+   *
+   * @return SQL statement
+   */
   private String getDisableStatement() {
     if (isPostgreSQL) {
       return "SELECT set_config('my.triggers_disabled','Y',true)";
@@ -82,6 +96,12 @@ public class RestCallTransactionHandlerImpl implements RestCallTransactionHandle
     }
   }
 
+  /**
+   * Returns the SQL statement to enable triggers. This statement is different depending on the
+   * database engine.
+   *
+   * @return SQL statement
+   */
   private String getEnableStatement() {
     if (isPostgreSQL) {
       return "SELECT set_config('my.triggers_disabled','N',true)";
