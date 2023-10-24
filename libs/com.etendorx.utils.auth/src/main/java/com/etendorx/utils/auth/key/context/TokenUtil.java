3b48d7a24d5895bb16352acd701379aed4269b68
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-10-24 11:13:13 -0300

    Feature EPL-1044: Change PR suggestions

--- libs/com.etendorx.utils.auth/src/main/java/com/etendorx/utils/auth/key/context/TokenUtil.java
+++ libs/com.etendorx.utils.auth/src/main/java/com/etendorx/utils/auth/key/context/TokenUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022-2023  Futit Services SL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.etendorx.utils.auth.key.context;
 
 import com.etendorx.utils.auth.key.JwtKeyUtils;
@@ -5,6 +20,16 @@ import com.etendorx.utils.auth.key.JwtKeyUtils;
 import java.util.Map;
 
 public class TokenUtil {
+
+  TokenUtil() {
+  }
+
+  /**
+   * Set the user context from the token
+   *
+   * @param publicKey the public key
+   * @param token     the token
+   */
   public static void convertToken(UserContext userContext, String publicKey, String token) {
     Map<String, Object> tokenValuesMap = ContextUtils.getTokenValues(publicKey, token);
     userContext.setUserId((String) tokenValuesMap.get(JwtKeyUtils.USER_ID_CLAIM));
