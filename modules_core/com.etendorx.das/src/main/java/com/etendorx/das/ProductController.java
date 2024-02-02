e5f6bbe1747e62fab8d832b3cacd33b91d0f769a
sebastianbarrozo <sebastian.barrozo@etendo.software>
2024-02-02 15:24:24 -0300

    Feature EPL-1263: RX UI module POC: Hilla

--- modules_core/com.etendorx.das/src/main/java/com/etendorx/das/ProductController.java
+++ modules_core/com.etendorx.das/src/main/java/com/etendorx/das/ProductController.java
@@ -1,6 +1,8 @@
 package com.etendorx.das;
 
 import com.etendorx.entities.jparepo.ProductRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.openbravo.model.common.plm.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,6 +17,10 @@ import static io.github.perplexhub.rsql.RSQLJPASupport.toSpecification;
 public class ProductController {
   @Autowired
   private ProductRepository productRepository;
+
+  @Operation(description = "Get products")
+  @Parameter(name = "search", description = "RSQL search string", example = "name==exampleProduct;price=gt=20")
+  @Parameter(name = "pageable", description = "Pagination", example = "{\n  \"page\": 0,\n  \"size\": 20,\n  \"sort\": [\n    \"id\"\n  ]\n}")
   @GetMapping("/sws/product")
   public Page<Product> product(@RequestParam(name = "search", required = false) String search, Pageable pageable) {
     return productRepository.findAll(toSpecification(search), pageable);
