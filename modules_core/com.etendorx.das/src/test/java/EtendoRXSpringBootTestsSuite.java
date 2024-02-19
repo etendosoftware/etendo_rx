2547baaacdfe343c625a135dc6efcd223eef2067
sebastianbarrozo <sebastian.barrozo@etendo.software>
2024-02-19 10:54:33 -0300

    Feature EPL-1315: Move rest tests to OBConnector

--- modules_core/com.etendorx.das/src/test/java/EtendoRXSpringBootTestsSuite.java
+++ modules_core/com.etendorx.das/src/test/java/EtendoRXSpringBootTestsSuite.java
@@ -1,5 +1,9 @@
 import com.etendorx.das.integration.PropertyMetadataTest;
-import com.etendorx.das.test.*;
+import com.etendorx.das.test.DefaultFiltersTest;
+import com.etendorx.das.test.DisableEnableTriggersTest;
+import com.etendorx.das.test.FieldMappingRestCallTest;
+import com.etendorx.das.test.RepositoryTest;
+import com.etendorx.das.test.RestCallTest;
 import com.etendorx.das.test.eventhandlertest.test.AnnotationTests;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
@@ -13,7 +17,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Suite
 @SuiteDisplayName("Etendo RX Spring Boot Tests Suite")
 @SelectClasses({ DefaultFiltersTest.class, DisableEnableTriggersTest.class,
-    FieldMappingRestCallTest.class, MappingsRestTest.class, PropertyMetadataTest.class,
+    FieldMappingRestCallTest.class, PropertyMetadataTest.class,
     RepositoryTest.class, RestCallTest.class,
     AnnotationTests.class
 })
