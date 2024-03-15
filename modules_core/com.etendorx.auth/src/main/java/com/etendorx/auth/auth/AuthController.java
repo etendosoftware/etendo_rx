b1a005d5e5860a138ed4d1fe6bc8b2d8dd2d76e3
Roman Magnoli <roman.magnoli@etendo.software>
2024-03-15 13:13:15 -0300

    Deleted unnesesary code.

--- modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/auth/AuthController.java
+++ modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/auth/AuthController.java
@@ -1,34 +1,18 @@
 package com.etendorx.auth.auth;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.etendorx.auth.auth.jwt.JwtRequest;
 import com.etendorx.auth.auth.jwt.JwtResponse;
 import com.etendorx.auth.auth.jwt.JwtService;
-import com.etendorx.auth.auth.utils.TokenInfo;
 import com.etendorx.auth.feign.model.UserModel;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
