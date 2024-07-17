f67538a4c24690544df95547346c5174ef813f94
Roman Magnoli <roman.magnoli@etendo.software>
2024-07-17 13:07:28 -0300

    Feature EPL-1417: Change the way to access to the HTML file in TokenController class.

--- modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/auth/TokenController.java
+++ modules_core/com.etendorx.auth/src/main/java/com/etendorx/auth/auth/TokenController.java
@@ -77,9 +77,10 @@ public class TokenController {
   }
 
   public String generateHtml(String title, String titleColor, String icon, String iconColor, String message) {
-    try {
-      Resource resource = resourceLoader.getResource("classpath:templates/oAuthResponse.html");
-      InputStream inputStream = resource.getInputStream();
+    try (InputStream inputStream = getClass().getResourceAsStream("/templates/oAuthResponse.html")) {
+      if (inputStream == null) {
+        throw new RuntimeException("Resource not found: templates/oAuthResponse.html");
+      }
       String html = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
       html = html.replace("{{title}}", title)
           .replace("{{titleColor}}", titleColor)
